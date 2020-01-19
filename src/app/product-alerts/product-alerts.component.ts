35e454dc81ede5d549f03109ab79ef7542ebf2e3
CamiloCuartas <camilocuartas@hotmail.com>
2020-01-19 11:50:33 -0500

    3 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+//Import Input from @angular/core.
+import { Input } from '@angular/core';
 
+//@Component() decorator. This indicates that the following class is a component
 @Component({
+  //The selector identifies the component. The selector is the name you give the Angular component when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app-, followed by the component name
   selector: 'app-product-alerts',
+  //The template and style filenames reference the HTML and CSS files that StackBlitz generates.
   templateUrl: './product-alerts.component.html',
+  //The template and style filenames reference the HTML and CSS files that StackBlitz generates.
   styleUrls: ['./product-alerts.component.css']
 })
+
 export class ProductAlertsComponent implements OnInit {
+
+  //The @Input() decorator indicates that the property value passes in from the component's parent, the product list component.
+  @Input() product;
 
   constructor() { }
 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+//importacion del array con la informacion de los productos
 import { products } from '../products';
 
 @Component({
@@ -8,7 +9,12 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  //exportamos a la vista la variable products la cual importamos
+  //desde el archivo ../products.ts
+  //products: array con la informacion importada
+  //product: variable local la cual vamos a exportar y la cual recibe
+  //el array de la variable products importada desde el archivo ../products.ts
+  product = products;
 
   share() {
     window.alert('The product has been shared!');
